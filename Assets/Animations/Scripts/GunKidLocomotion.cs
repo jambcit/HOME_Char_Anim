52892bdacf81e316de7e50b379d87d4eaa572c16
Ching Choi <chingchoi128@gmail.com>
2019-01-27 03:00:37 -0800

    initial upload for animated models

---
+++ Assets/Animations/Scripts/GunKidLocomotion.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunKidLocomotion : MonoBehaviour
+{
+
+    private Animator _gunAnimator;
+    private const float timer = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gunAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_gunAnimator == null) return;
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+        Move(x, y);
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (_gunAnimator.GetBool("Crouching"))
+            {
+                _gunAnimator.SetBool("Crouching", false);
+            }
+            else
+            {
+                _gunAnimator.SetBool("Crouching", true);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _gunAnimator.SetTrigger("GunJump");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _gunAnimator.Play("GunShooting", -1, 0);
+        }
+    }
+
+    private void Move(float x, float y)
+    {
+        _gunAnimator.SetFloat("VelX", x);
+        _gunAnimator.SetFloat("VelY", y);
+    }
+    
+}
---
+++ Assets/Animations/Scripts/KidLocomotion.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KidLocomotion : MonoBehaviour {
+
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_animator == null) return;
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+        Move(x, y);
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            _animator.SetTrigger("Pickup");
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _animator.SetTrigger("Punch");
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (_animator.GetBool("Crouching"))
+            {
+                _animator.SetBool("Crouching", false);
+            }
+            else
+            {
+                _animator.SetBool("Crouching", true);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger("Jump");
+        }
+    }
+
+    private void Move(float x, float y)
+    {
+        _animator.SetFloat("VelX", x);
+        _animator.SetFloat("VelY", y);
+    }
+}
